71e42367bbcdb13b1c44e2f967ba31da6aa0cc7b
keaoyao <keaoyao@gmail.com>
2022-07-27 18:28:13 +0800

    init project

---
+++ src/main/java/com/example/proj5WebDemo/Proj5WebDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.proj5WebDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj5WebDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj5WebDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proj5WebDemo/Proj5WebDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proj5WebDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proj5WebDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
